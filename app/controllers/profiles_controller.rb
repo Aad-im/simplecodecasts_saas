ac304fb994f3d6d6314121b4040ab22294decb70
Whitley Horn <whitleyblaine@gmail.com>
2016-01-15 22:36:11 +0000

    Added form partial and edit action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -13,8 +13,12 @@ class ProfilesController < ApplicationController
       redirect_to user_path( params[:user_id] )  
     else
       render action: :new
-      
     end
+  end
+  
+  def edit
+    @user = User.find( params[:user_id] )
+    @profile = @user.profile
   end
   
   private
